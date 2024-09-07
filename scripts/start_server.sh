a9594166b3cfb5e732240a658281525e7ae4b6be
Sudarshanas232001 <76169089+Sudarshanas232001@users.noreply.github.com>
2024-09-07 10:37:36 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# Navigate to the application directory
 cd /home/ec2-user/nodejs-app
-pm2 start app.js --name "nodejs-app"
+
+# Start the application
+npm start > /home/ec2-user/nodejs-app/app.log 2>&1 &
