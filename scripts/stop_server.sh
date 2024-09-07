2d0a3a62eac02190493d7f40c92dbed9284fdd86
Sudarshanas232001 <76169089+Sudarshanas232001@users.noreply.github.com>
2024-09-07 12:33:11 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-# Stop any existing application processes
+echo "Starting BeforeInstall script" >> /home/ec2-user/nodejs-app/app.log
 if pgrep -f 'node src/index.js'; then
+  echo "Stopping existing application processes" >> /home/ec2-user/nodejs-app/app.log
   pkill -f 'node src/index.js'
+else
+  echo "No running application processes found" >> /home/ec2-user/nodejs-app/app.log
 fi
+echo "Completed BeforeInstall script" >> /home/ec2-user/nodejs-app/app.log
